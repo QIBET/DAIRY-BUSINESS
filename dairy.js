d9dc3b194cd81a22184edb091de5c524588c79cf
bernard ngetich <bernardonline88@gmail.com>
2021-03-13 17:47:26 +0300

    Addition of function and data input code

--- dairy.js
+++ dairy.js
@@ -1,1 +1,24 @@
-var shed = [A, B, C, D];
+var generate = document.getElementById("generate");
+generate.addEventListener("click", totalProduction);
+
+function = totalProduction() {
+
+    var shedA = document.getElementById("shedA").value;
+    var shedB = document.getElementById("shedB").value;
+    var shedC = document.getElementById("shedC").value;
+    var shedD = document.getElementById("shedD").value;
+     return(shedA + shedB + shedC + shedD);
+
+   console.log()
+
+    /*if(!shedA || !shedB || !shedC || !shedD) 
+    {
+        alert("Please enter litres of all Sheds");
+        
+        //return;
+    }
+    else{
+       
+        alert(generate);
+    }*/
+}
