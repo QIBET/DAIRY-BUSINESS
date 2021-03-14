9b372c3596553eba6fe69e8b7a522cb322e9cacc
bernard ngetich <bernardonline88@gmail.com>
2021-03-14 11:15:07 +0300

    Modification of iteration code

--- dairy.js
+++ dairy.js
@@ -1,7 +1,7 @@
 var generate = document.getElementById("generate");
 generate.addEventListener("click", totalProduction);
 
-function = totalProduction() {
+var totalProduction = function() {
 
     var shedA = document.getElementById("shedA").value;
     var shedB = document.getElementById("shedB").value;
@@ -10,16 +10,13 @@ function = totalProduction() {
 
    totalProduction(shedA + shedB + shedC + shedD);
    var sheds = [shedA, shedB, shedC, shedD];
-   for(var i = 0; i = sheds.length; i++){
+   for (var i = 0; i = sheds.length; i++){
        console.log("Your production in Shed A" +sheds[i]+ "litres per day ");
-       if{
-           sheds.length === 5;
+       if (sheds.length === 5) {
+           
+
            console.log("The total production is "+totalProduction()+"litres per day");
        }
-
-   }
     
-}
-else{
-    console.log("Kindly confirm your figures")
+   }
 }
