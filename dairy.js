b872732d3216c94a1c0972495de28ca990892707
bernard ngetich <bernardonline88@gmail.com>
2021-03-14 20:29:31 +0300

    Additional code to calculate

--- dairy.js
+++ dairy.js
@@ -1,22 +1,32 @@
-var generate = document.getElementById("generate");
-generate.addEventListener("click", totalProduction);
+function totalProduction(a,b,c,d){
+    return a + b + c + d;
+}
+function doMath() {
 
-var totalProduction = function() {
+    var shedA = Number(document.getElementById('shedA').value);
+    var shedB = Number(document.getElementById('shedB').value);
+    var shedC = Number(document.getElementById('shedC').value);
+    var shedD = Number(document.getElementById('shedD').value);
 
-    var shedA = document.getElementById("shedA").value;
-    var shedB = document.getElementById("shedB").value;
-    var shedC = document.getElementById("shedC").value;
-    var shedD = document.getElementById("shedD").value;
+   var sheds = totalProduction (shedA,shedB,shedC,shedD)
 
-   totalProduction(shedA + shedB + shedC + shedD);
+  
+}
+document.getElementById('generate').addEventListener('click', doMath);
+
+   
+   /*   
    var sheds = [shedA, shedB, shedC, shedD];
+
    for (var i = 0; i = sheds.length; i++){
+
        console.log("Your production in Shed A" +sheds[i]+ "litres per day ");
+       
        if (sheds.length === 5) {
            
 
            console.log("The total production is "+totalProduction()+"litres per day");
        }
     
-   }
-}
+   }*/
+   
